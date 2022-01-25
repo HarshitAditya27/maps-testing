621536ba78af350bcf890e90216a0be384968e50
Harshit <harshitaditya27@gmail.com>
2022-01-25 21:40:07 +0530

    Added Files

---
+++ script.js
@@ -0,0 +1,7 @@
+function initMap() {
+    new google.maps.Map(document.getElementById("map"), {
+        mapId: "7fe0ece5a9153e22",
+        center: { lat: 48.85, lng: 2.35 },
+        zoom: 12,
+    });
+}
